5e54ae187066c086080c3cd2252bea32ac010cbe
Grzegorz Lipecki <glipecki@consdata.pl>
2020-08-18 13:39:28 +0200

    (wip) adds user registration (use FieldPath for firestore query)

--- apps/functions/src/app/register-user/register-user.handler.ts
+++ apps/functions/src/app/register-user/register-user.handler.ts
@@ -3,29 +3,33 @@ import {RegisterUserRequest} from './register-user.request';
 const emailRegex = /^[a-zA-Z0-9._-]+@(?<domain>[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})$/i;
 
 export const registerUserFactory = (functions: import('firebase-functions').FunctionBuilder, firebase: typeof import('firebase-admin')) =>
-    functions.https.onRequest(async (request, response) => {
-        console.info(`Request for user registration (${JSON.stringify(request.body)})`);
+  functions.https.onRequest(async (request, response) => {
+    console.info(`Request for user registration (${JSON.stringify(request.body)})`);
 
-        if (request.method !== 'POST') {
-            return response.status(405).send('Invalid request method (only POST allowed)');
-        }
+    if (request.method !== 'POST') {
+      return response.status(405).send('Invalid request method (only POST allowed)');
+    }
 
-        const registerRequest = request.body as RegisterUserRequest;
-        const emailMatch = registerRequest?.email?.match(emailRegex);
-        const emailDomain = emailMatch[1];
-        console.debug(`Registering user for email domain: ${emailDomain}`);
+    const registerRequest = request.body as RegisterUserRequest;
+    const emailMatch = registerRequest?.email?.match(emailRegex);
+    const emailDomain = emailMatch[1];
 
-        const invitations = firebase.firestore().collection('invitation');
-        const invitation = await invitations.where(`domain."${emailDomain}"`, '==', true).get();
+    const invitations = firebase.firestore().collection('invitation');
+    const invitation = await invitations.where(
+      new firebase.firestore.FieldPath('domain', emailDomain),
+      '==',
+      true
+    ).get();
 
-        if (invitation.empty) {
-            return response.contentType('json')
-                .status(400)
-                .send({status: 'bad'});
-        } else {
-            console.log(invitation.docs);
-            return response.contentType('json')
-                .status(200)
-                .send({status: 'ok'})
-        }
-    });
+    if (invitation.empty) {
+      console.info(`Invitation not found for domain: ${emailDomain}`);
+      return response.contentType('json')
+        .status(400)
+        .send({status: 'bad'});
+    } else {
+      // TODO: create user
+      return response.contentType('json')
+        .status(200)
+        .send({status: 'ok'})
+    }
+  });
