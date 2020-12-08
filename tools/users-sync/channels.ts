e0a9746379d9a598c0bb7aa70308a3df5a43be44
Grzgorz Lipecki <grzegorz.lipecki@gmail.com>
2020-12-08 10:58:52 +0100

    changes channel manager id to email

--- tools/users-sync/channels.ts
+++ tools/users-sync/channels.ts
@@ -2,13 +2,16 @@ import {ChannelDocument} from "./channel-document";
 import {directory} from "./users/directory";
 
 export const channels: ChannelDocument[] = Object.values(directory.channels).map(
-  channel => ({
-    display_name: channel.display_name,
-    managers: channel.managers,
-    managerMap: channel.managers.reduce((map, manager) => ({
-      ...map,
-      [manager]: true
-    }), {}),
-    slackChannel: channel.slackChannel
-  })
+  channel => {
+    const managers = channel.managers.map(manager => directory.users[manager].email);
+    return {
+      display_name: channel.display_name,
+      managers: managers,
+      managerMap: managers.reduce((map, manager) => ({
+        ...map,
+        [manager]: true
+      }), {}),
+      slackChannel: channel.slackChannel
+    };
+  }
 )
