83a63ca4abd04b3a479d6bf66ee032a4fdb3f564
Grzgorz Lipecki <grzegorz.lipecki@gmail.com>
2020-12-09 14:19:05 +0100

    adds inbox stats for user

--- apps/functions/src/main.ts
+++ apps/functions/src/main.ts
@@ -13,6 +13,9 @@ import {storeUserFeedbackFactory} from './app/store-user-feedback/store-user-fee
 import {userFeedbackStatsFactory} from './app/user-feedback-stats/user-feedback-stats-factory';
 import {storeChannelFeedbackHandlerFactory} from "./app/store-channel-feedback/store-channel-feedback.handler";
 import {notifyAfterChannelFeedbackFactory} from "./app/notify-after-channel-feedback/notify-after-channel-feedback.handler";
+import {updateFilterStatsAfterInboxCreateFactory} from "./app/update-filter-stats/update-filter-stats-after-inbox-create";
+import {updateFilterStatsAfterInboxChangeFactory} from "./app/update-filter-stats/update-filter-stats-after-inbox-change";
+import {updateFilterStatsAfterInboxDeleteFactory} from "./app/update-filter-stats/update-filter-stats-after-inbox-delete";
 
 firebase.initializeApp();
 
@@ -33,6 +36,9 @@ export const userFeedbackStats = userFeedbackStatsFactory(functionBuilder(), fir
 export const createUser = createUserFactory(functionBuilder(), firebase);
 export const expireUserAccounts = expireUserAccountsFactory(functionBuilder(), firebase);
 export const getChapterStats = getChapterStatsFactory(functionBuilder(), firebase);
+export const updateFilterStatsAfterInboxCreate = updateFilterStatsAfterInboxCreateFactory(functionBuilder(), functions.config(), firebase);
+export const updateFilterStatsAfterInboxChange = updateFilterStatsAfterInboxChangeFactory(functionBuilder(), functions.config(), firebase);
+export const updateFilterStatsAfterInboxDelete = updateFilterStatsAfterInboxDeleteFactory(functionBuilder(), functions.config(), firebase);
 export const kudosHandler = kudosHandlerFactory(
   functionBuilder().runWith({memory: '512MB'}),
   functions.config(),
