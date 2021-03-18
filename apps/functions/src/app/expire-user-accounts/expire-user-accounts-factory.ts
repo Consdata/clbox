ea85c5a7ed611ba3defaaa5f5a659e2a769cf01b
Grzegorz <grzegorz.lipecki@gmail.com>
2021-03-18 10:31:40 +0100

    Fixes exception for missing expired accounts.

--- apps/functions/src/app/expire-user-accounts/expire-user-accounts-factory.ts
+++ apps/functions/src/app/expire-user-accounts/expire-user-accounts-factory.ts
@@ -1,55 +1,67 @@
 export const expireUserAccountsFactory = (
-  functions: import('firebase-functions').FunctionBuilder,
-  firebase: typeof import('firebase-admin')
+    functions: import('firebase-functions').FunctionBuilder,
+    firebase: typeof import('firebase-admin')
 ) => {
-  const firestore = firebase.firestore();
-  const fetchTeams = async () => {
-    const teams = await firestore.collection(`team`).get();
-    return teams.docs.map(doc => doc.id);
-  };
-  const expiringUsers = async (team: string) => {
-    return await firestore.collection(`team/${team}/user`)
-      .where('withExpire', '==', true)
-      .get();
-  }
-
-  function isActive(user) {
-    const expire = user.expireDate;
-    if (!expire) {
-      return true;
+    const firestore = firebase.firestore();
+    const fetchTeams = async () => {
+        const teams = await firestore.collection(`team`).get();
+        return teams.docs.map(doc => doc.id);
+    };
+    const expiringUsers = async (team: string) => {
+        return await firestore.collection(`team/${team}/user`)
+            .where('withExpire', '==', true)
+            .get();
     }
-    return new Date().getTime() < new Date(expire).getTime();
-  }
 
-  return functions.pubsub.schedule('0 3 * * 1-7')
-    .timeZone('Europe/Warsaw')
-    .onRun(async ctx => {
-      const teams = await fetchTeams();
-      await Promise.all(teams.map(async team => {
-        const users = await expiringUsers(team);
-        await Promise.all(users.docs
-          .filter(user => !isActive(user.data()))
-          .map(async user => {
-            console.log(`User to disable: ${user.id} with expire date ${user.data().expireDate}`);
+    function isActive(user) {
+        const expire = user.expireDate;
+        if (!expire) {
+            return true;
+        }
+        return new Date().getTime() < new Date(expire).getTime();
+    }
 
-            const authUser = await firebase.auth().getUserByEmail(user.id);
-            await firebase.auth().updateUser(authUser.uid, {
-              disabled: true
-            });
+    async function tryGetUserByEmail(email: string) {
+        try {
+            return await firebase.auth().getUserByEmail(email);
+        } catch {
+            return null;
+        }
+    }
 
-            const userDoc = firestore.collection(`team/${team}/user/`).doc(user.id);
-            await firestore.runTransaction(async trn => {
-              const existingUserDoc = await trn.get(userDoc);
-              trn
-                .delete(userDoc)
-                .create(
-                  firestore.collection(`team/${team}/removed-users/`).doc(existingUserDoc.id),
-                  existingUserDoc.data()
+    return functions.pubsub.schedule('0 3 * * 1-7')
+        .timeZone('Europe/Warsaw')
+        .onRun(async ctx => {
+            const teams = await fetchTeams();
+            await Promise.all(teams.map(async team => {
+                const users = await expiringUsers(team);
+                await Promise.all(users.docs
+                    .filter(user => !isActive(user.data()))
+                    .map(async user => {
+                        const authUser = await tryGetUserByEmail(user.id);
+                        if (authUser) {
+                            console.log(`User to disable: ${user.id} with expire date ${user.data().expireDate}`);
+                            console.log(authUser);
+
+                            await firebase.auth().updateUser(authUser.uid, {
+                                disabled: true
+                            });
+
+                            const userDoc = firestore.collection(`team/${team}/user/`).doc(user.id);
+                            console.log(userDoc);
+                            await firestore.runTransaction(async trn => {
+                                const existingUserDoc = await trn.get(userDoc);
+                                trn
+                                    .delete(userDoc)
+                                    .create(
+                                        firestore.collection(`team/${team}/removed-users/`).doc(existingUserDoc.id),
+                                        existingUserDoc.data()
+                                    );
+                            });
+                        }
+                    })
                 );
-            });
-          })
-        );
-      }));
-      return null;
-    })
+            }));
+            return null;
+        })
 };
